ed9c521634dc2e8c68a2ed3bbf6c6c0b984962c6
buaa-wuhao <buaa_wuhao@163.com>
2018-06-09 00:44:07 -0500

    Utilize Smart Pointer to store string info and line index

--- C++_Programming/C++_Primer/A_Text_Query_Program/source/TextQuery.cpp
+++ C++_Programming/C++_Primer/A_Text_Query_Program/source/TextQuery.cpp
@@ -1,33 +1,28 @@
 #include "TextQuery.h"
 
-TextQuery::TextQuery(ifstream input)
+TextQuery::TextQuery(ifstream& input)
 {
 	string line;
 	while(getline(input,line))
 	{
-		lines.push_back(line);
+//		cout<<line<<endl;
+		lines->push_back(line);
 		int idx = lines.size()-1;
 		istringstream iss(line);
 		string word;
 
                 while(iss >> word)
 		{
-		    str2line[word].push_back(idx);
+		    str2line[word]->push_back(idx);
 		}
 
 	}
 }
 
 
-void Query(string word)
+QueryResult TextQuery::Query(string word)
 {
-	if(str2line.count(word)==0) return;
-	cout << "element occurs " << str2line[word].size() << "times" << endl;
-	
-	for(int i=0;i<str2line[word].size();i++)
-	{
-	    cout<<"line "<<str2line[word][i]<<" "<<lines[str2line[word][i]] << endl;
-	}
 
-	return;
+	QureyResult QR(lines,str2line[word]);
+	return QR;
 }
--- C++_Programming/C++_Primer/A_Text_Query_Program/source/main.cpp
+++ C++_Programming/C++_Primer/A_Text_Query_Program/source/main.cpp
@@ -2,8 +2,18 @@
 
 int main(int argc, char* argv[])
 {
-	TextQuery tq(argv[1]);
-	string word(argv[2]);
-	tq.Query();
+	ifstream input(argv[1]);
+	TextQuery tq(input);
+	string word;
+
+	while((cin>>word) || word!="q")
+	{
+            QueryResult qr = tq.Query(word);
+	    for(int i=0;i<qr.rows->size();i++)
+	    {
+		cout<< *(qr.lines->begin()+ *(qr.rows->begin()+i)) << endl;
+	    }
+	}	
+	
 	return 0;
 }
