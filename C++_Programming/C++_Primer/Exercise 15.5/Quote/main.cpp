0fcc29fcf6fec80a617eab58de056b9ac0a360de
buaa-wuhao <buaa_wuhao@163.com>
2018-09-10 23:09:46 -0500

    day day up

--- C++_Programming/C++_Primer/Exercise 15.5/Quote/main.cpp
+++ C++_Programming/C++_Primer/Exercise 15.5/Quote/main.cpp
@@ -6,9 +6,21 @@
 
 using namespace std;
 
+void print_total(ostream &os, const Quote& item, size_t n)
+{
+    os<<item.net_price(n) << endl;
+    return;
+}
+
 int main()
 {
     cout << "Hello world!" << endl;
-    Quote quote("book",3);
+    //Disc_Quote dq("dq_book",3.5,8,0.2);
+    Quote quote("book",3.3);
+    quote.debug();
+    Bulk_Quote bq("d_book",3.3,6,0.2);
+    bq.debug();
+    //print_total(cout,quote,4);
+    //print_total(cout,bq,10);
     return 0;
 }
